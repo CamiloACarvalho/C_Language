be5e629a92b51f503f9237b3a04115ea1ac3a2ab
Camilo Augusto Carvalho <111397870+CamiloACarvalho@users.noreply.github.com>
2022-10-15 15:16:58 -0300

    Add files via upload

---
+++ Exercício 24-08.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //*Exercicio de laboratorio de programacao/
+    //*n=1
+    //float x,y;
+    //printf("\n digite dois numeros quais quer\n");
+    //scanf("%f %f",&x,&y);
+    //x/y;
+    //if (y>0)
+    //printf("\n sao validos\n");
+    //else
+    //printf("\n nao eh possivel\n");
+
+    //*n=2
+    //float x,y;
+    //printf("\n Digite dois valores reias quais quer:\n");
+    //scanf("%f %f,",&x,&y);
+    //if (x&&y>0)
+    //{
+      //printf("\n valores validos\n");
+    //}
+    //else
+    //printf("\n valores invalidos\n");
+
+    //*n=3
+    float h,x,y;
+    printf("Digite 1 para homem e 2 para mulher.\n");
+    scanf("%f",&x);
+    printf("Agora digite o valor de sua altura:\n");
+    scanf("%f",&h);
+    if(x==1)
+    {
+    y=(72.7*h)-58;
+    printf("o valor do seu peso e':%f",y);
+    }
+    if(x==2)
+    {
+    y=(62.1*h)-44.7;
+    printf("o valor do seu peso e':%f",y);
+}
+   if(x!=1&&x!=2)
+   {
+       printf("valor invalido");
+    }
+
+    //n=4
+    //float x,y,z;
+    //printf("digite tres numeross inteiros:\n");
+    //scanf("%f %f %f",&x,&y,&z);
+    //if(y<x+y||z<x+y||x<y+z)
+    //{
+        //printf("forma triangulo\n");
+    //}
+    //else
+    //{
+        //printf("nao forma triangulo\n");
+    //}
+
+    //float c,s,x;
+    //printf("Digite 1 para tecnico, 2 para gerente e 3 para funcionario:\n");
+    //scanf("%f",&c);
+    //printf("Agora digite o valor do seu salario:\n");
+    //scanf("%f",&s);
+    //if(c==1)
+    //{
+    //    x=s*1.5;
+    //    printf("o teu novo salario e':\n %.2f",x);
+    //}
+    //if(c==2)
+    //{
+    //    x=s*1.3;
+    //    printf("o teu novo salario e':\n %.2f",x);
+    //}
+    //if(c==3)
+    //{
+    //    x=s*1.2;
+    //    printf("o teu novo salario e':\n %.2f",x);
+    //}
+    //if(c!=1||c!=2||c!=3)
+    //{
+    //    printf("o numero nao corresponde aos cargos.\n");
+    //}
+
+    return 0;
+}
