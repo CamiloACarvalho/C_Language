33e73d3dc36c35e5d280cd8cb2f5a4c7f811b22f
Camilo Augusto Carvalho <111397870+CamiloACarvalho@users.noreply.github.com>
2022-10-18 22:18:07 -0300

    Add files via upload

---
+++ Dan.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i;
+    for(i=1;i<1001;i++)
+     {
+         if((i%13==0)&&(i%17==0))
+
+           {
+               printf("%d\n",i);
+           }
+     }
+
+
+    return 0;
+}
