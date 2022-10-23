117ff1c0e1ddcccbff08034f62d832c2694b7a58
Camilo Augusto Carvalho <111397870+CamiloACarvalho@users.noreply.github.com>
2022-10-23 10:40:26 -0300

    Add files via upload

---
+++ Prova1.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define tamanho 10
+
+/*
+int main()
+{
+    int vet[10],a,b,x=0, menornum;
+    for (a=0;a<10;a++)
+    {
+        printf("\n Digite um numero inteiro qualquer para assumir a posica %d do vetor: ",x);
+        scanf("%d",&vet[a]);
+        x++;
+
+          if(vet[a]<menornum)
+            {
+                menornum=vet[a];
+                if(menornum=vet[a])
+                {
+                    b=a;
+
+                }
+            }
+        }
+
+printf ("\n o menor numero eh :%d e a posicao eh:%d", menornum, b );
+return 0;
+}
+*/
+
+//numero 15
+/*
+int pert(int vet[], int x)
+{
+    int i;
+    for(i=0;i<10;i++)
+    {
+        if(vet[i]==x)
+        {
+         return 1;
+        }
+    }
+    return 0;
+}
+
+int main()
+{
+    int vetor[tamanho], a,b,h,y=0;
+    for (a=0;a<tamanho;a++)
+    {
+        printf("\n Digite um numero inteiro, diferente de zero, na posicao %d do vetor: ",y);
+        scanf("%d",&vetor[a]);
+        y++;
+    }
+    printf("\nDigite um numero inteiro a ser procurado no vetor: \n");
+    scanf("%d",&h);
+    b=pert(vetor,h);
+    if (b==1)
+    printf ("%d pertence ao vetor",h);
+    else
+    {
+        printf ("nao pertence");
+    }
+    return 0;
+}
+*/
