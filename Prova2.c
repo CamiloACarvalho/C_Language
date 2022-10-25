c77b9ec07894d6518a397f30bb5f65c97d93e573
Camilo Augusto Carvalho <111397870+CamiloACarvalho@users.noreply.github.com>
2022-10-24 22:01:07 -0300

    Add files via upload

---
+++ Prova2.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   //numero 3
+   //int x,min;
+   //printf("\n Digite um minuto interio qualquer:\n");
+   //scanf("%d",&min);
+   //if(min>=0)
+   //{
+   //    x=min*60;
+   //    printf("\n%d equivale a %d segundos.",min,x);
+   //}
+   //else
+   //printf("\n Valor invalido.\n");
+
+   //numero 4
+   //int cargo;
+   //printf("\nDigite 1 para Diretor, 2 para Gerente, 3 para Chefe e 4 para Funcionario.\n");
+   //scanf("%d",&cargo);
+   //switch(cargo)
+   //{
+   //    case 1: printf("\nDIRETOR\n");
+   //    break;
+   //    case 2: printf("\nGERENTE\n");
+   //    break;
+   //    case 3: printf("\nCHEFE\n");
+   //    break;
+   //    case 4: printf("\nFUNCIONARIO\n");
+   //    break;
+   //    default: printf("\nCARGO INVALIDO\n");
+   //}
+
+   //int b;
+   //printf("\n Os valores de 1 a 10000 divisiveis por 13 e 17 ao mesmmo tempo sao:\n");
+   //for(b=1;b<=10000;b++)
+   //{
+   //     if((b%13==0)&&(b%17==0))
+   //     {
+   //         printf("\n %d",b);
+   //     }
+   //}
+
+    return 0;
+}
