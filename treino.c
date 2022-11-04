117fa9d8cac39ae1684641459cf6dc0502568734
Camilo Augusto Carvalho <111397870+CamiloACarvalho@users.noreply.github.com>
2022-11-04 17:48:29 -0300

    Add files via upload

---
+++ treino.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Camilo Augusto Carvalho\n");
+    printf("\t\t\tIdade: 20 anos\n");
+    printf("\t\t\tAltura:1,81 metros\n\n");
+    printf("%.1f\n",2.346728);
+    printf("%.2f\n",2.346728);
+    printf("%.3f\n",2.346728);
+    printf("%.5f\n",2.346728);
+    return 0;
+}
