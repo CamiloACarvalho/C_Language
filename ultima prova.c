a42d673ee7109c642fedb64830747c899ceb62eb
Camilo Augusto Carvalho <111397870+CamiloACarvalho@users.noreply.github.com>
2022-11-05 18:40:11 -0300

    Add files via upload

---
+++ ultima prova.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+typedef struct
+{
+    char nome[100];
+    int idade;
+}dado;
+int main()
+{
+    int i,j;
+    dado a[3];
+    for(i=0;i<3;i++)
+    {
+        printf("\n Digite o nome: ");
+        gets(a[i].nome);
+        scanf("%*c");
+        printf("\n Digite a idade: ");
+        scanf("%d",&a[i].idade);
+        scanf("%*c");
+    }
+    for(j=0;j<3;j++)
+    {
+        printf("\n Nome: ");
+        puts(a[j].nome);
+        printf("\n Idade: %d",a[j].idade);
+
+    }
+    return 0;
+}
+*/
